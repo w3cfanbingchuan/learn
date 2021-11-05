70e38bf01536757f31f5659f700a3495559e7701
fanbingchuan <m13671335217@163.com>
2021-11-05 18:27:01 +0800

    part1-2

---
+++ part1-2/part1-7.js
@@ -0,0 +1,13 @@
+let promise = new Promise(function(resolve,reject){
+  setTimeout(function(){
+    resolve(100)
+  },1000)
+  
+  // reject('is error')
+})
+promise.then(function(res){
+  console.log('success',res)
+},function(err){
+  console.log('error:',err)
+})
+console.log(111111)
